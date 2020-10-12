b8cb4736c732f06340a443689d61bd65ce05e6d9
Adam Napoletano <adam.napps@gmail.com>
2020-10-12 03:49:34 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,35 @@
 # Write your methods here
+def loop_message_five_times(string)
+puts string
+puts string
+puts string
+puts string
+puts string
+end
+
+def loop_message_n_times(message, input)
+counter = 0
+while counter < input
+  puts message
+  counter += 1
+end
+end
+
+
+def output_array(array)
+  counter = 0
+  while counter < array.length
+    puts array[counter]
+    counter += 1
+  end
+end
+
+def return_string_array(array)
+  counter = 0
+  while counter < array.length
+    puts array[counter].to_s
+    counter += 1
+  end
+
+
+end
